502f87473b632ba7e4c080f0d78de10a97c13548
colombovf <colombovf@gmail.com>
2022-11-10 17:06:41 +0100

    Correzione Nome Variabili

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //Dichiaro tutte le variabili
 let numero_km = prompt("Quanti km vuole percorrere?");
 let numero_eta = prompt("Quanti anni ha?");
-let ticket_price = 0.21 * numkm;
+let ticket_price = 0.21 * numero_km;
 //Massimo due cifre dopo la virgola
 ticket_price = ticket_price.toFixed(2);
 
 //
-if (numeta<18) {
+if (numero_eta<18) {
     let ticket_sconto = ticket_price - (ticket_price * 0.2);
     ticket_sconto = ticket_sconto.toFixed(2);
     console.log("Il prezzo totale del suo biglietto sarà: " + ticket_sconto + "€");
 }
-else if (numeta>65) {
+else if (numero_eta>65) {
     let ticket_sconto = ticket_price - (ticket_price * 0.4);
     ticket_sconto = ticket_sconto.toFixed(2);
     console.log("Il prezzo totale del suo biglietto sarà: " + ticket_sconto + "€");
