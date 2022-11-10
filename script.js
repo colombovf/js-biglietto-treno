0e92b62c98376ca7bf864c758017f20ff3bfd634
colombovf <colombovf@gmail.com>
2022-11-10 16:10:53 +0100

    Nomi estesi delle variabili

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 //Dichiaro tutte le variabili
-let numkm = prompt("Quanti km vuole percorrere?");
-let numeta = prompt("Quanti anni ha?");
-let tickprice = 0.21 * numkm;
+let numero_km = prompt("Quanti km vuole percorrere?");
+let numero_eta = prompt("Quanti anni ha?");
+let ticket_price = 0.21 * numkm;
 //Massimo due cifre dopo la virgola
-tickprice = tickprice.toFixed(2);
+ticket_price = ticket_price.toFixed(2);
 
 //
 if (numeta<18) {
-    let ticksconto = tickprice - (tickprice * 0.2);
-    ticksconto = ticksconto.toFixed(2);
-    console.log("Il prezzo totale del suo biglietto sarà: " + ticksconto + "€");
+    let ticket_sconto = ticket_price - (ticket_price * 0.2);
+    ticket_sconto = ticket_sconto.toFixed(2);
+    console.log("Il prezzo totale del suo biglietto sarà: " + ticket_sconto + "€");
 }
 else if (numeta>65) {
-    let ticksconto = tickprice - (tickprice * 0.4);
-    ticksconto = ticksconto.toFixed(2);
-    console.log("Il prezzo totale del suo biglietto sarà: " + ticksconto + "€");
+    let ticket_sconto = ticket_price - (ticket_price * 0.4);
+    ticket_sconto = ticket_sconto.toFixed(2);
+    console.log("Il prezzo totale del suo biglietto sarà: " + ticket_sconto + "€");
 }
 else {
-    console.log("Il prezzo totale del suo biglietto sarà: " + tickprice);
+    console.log("Il prezzo totale del suo biglietto sarà: " + ticket_price);
 }
 
